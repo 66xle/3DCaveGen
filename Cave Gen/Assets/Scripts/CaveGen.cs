b679c4fb06e0903b3605ba2b370c96351681b7d5
Samuel <robbo_rules1@live.com.au>
2021-04-28 17:14:36 +1000

    Worm moving in 3d space

--- Cave Gen/Assets/Scripts/CaveGen.cs
+++ Cave Gen/Assets/Scripts/CaveGen.cs
@@ -28,10 +28,12 @@ public class CaveGen : MonoBehaviour
     public float twistness = (4.0f / 256.0f);
 
     ModuleBase module;
-    private float value;
+    ModuleBase module2;
+    private float valueA;
+    private float valueB;
 
     private Vector3 headNoisePos;
-    private Vector2 headScreenPos;
+    private Vector3 headScreenPos;
     
 
     List<GameObject> wormSegments;
@@ -42,6 +44,7 @@ public class CaveGen : MonoBehaviour
         wormSegments = new List<GameObject>();
 
         module = new Perlin(1.0, 2.375, 0.5, 3, seed, QualityMode.Low);
+        module2 = new Perlin(1.0, 2.375, 0.5, 3, seed + 1, QualityMode.Low);
 
         headNoisePos = new Vector3((7.0f / 2048.0f), (1163.0f / 2048.0f), (409.0f / 2048.0f));
 
@@ -50,12 +53,13 @@ public class CaveGen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        value = (float)module.GetValue(headNoisePos.x, headNoisePos.y, headNoisePos.z);
+        valueA = (float)module.GetValue(headNoisePos.x, headNoisePos.y, headNoisePos.z);
+        valueB = (float)module2.GetValue(headNoisePos.x, headNoisePos.y, headNoisePos.z);
 
         // Direction the worm moves in (opposite direction)
-
-        headScreenPos.x -= (Mathf.Cos(value * 2.0f * Mathf.PI) * speed);
-        headScreenPos.y -= (Mathf.Sin(value * 2.0f * Mathf.PI) * speed);
+        headScreenPos.x -= (Mathf.Cos(valueA * 2.0f * Mathf.PI) * Mathf.Sin(valueB * 2.0f * Mathf.PI) * speed);
+        headScreenPos.y -= (Mathf.Sin(valueA * 2.0f * Mathf.PI) * Mathf.Sin(valueB * 2.0f * Mathf.PI) * speed);
+        headScreenPos.z -= (Mathf.Cos(valueB * 2.0f * Mathf.PI) * speed);
 
         headNoisePos.x -= speed * 2;
         headNoisePos.y += lateralSpeed;
@@ -74,8 +78,8 @@ public class CaveGen : MonoBehaviour
         }
         wormSegments.Clear();
 
-        Vector2 curSegmentScreenPos = headScreenPos;
-        Vector2 offsetPos;
+        Vector3 curSegmentScreenPos = headScreenPos;
+        Vector3 offsetPos;
 
         Vector3 curNoisePos;
 
@@ -85,12 +89,14 @@ public class CaveGen : MonoBehaviour
             curNoisePos.x = headNoisePos.x + (curSegment * twistness);
             curNoisePos.y = headNoisePos.y;
             curNoisePos.z = headNoisePos.z;
-            float noiseValue = (float)module.GetValue(curNoisePos.x, curNoisePos.y, curNoisePos.z);
+            float noiseValueA = (float)module.GetValue(curNoisePos.x, curNoisePos.y, curNoisePos.z);
+            float noiseValueB = (float)module.GetValue(curNoisePos.x, curNoisePos.y, curNoisePos.z);
 
-            offsetPos.x = Mathf.Cos(noiseValue * 2.0f * Mathf.PI);
-            offsetPos.y = Mathf.Sin(noiseValue * 2.0f * Mathf.PI);
+            offsetPos.x = Mathf.Cos(noiseValueA * 2.0f * Mathf.PI) * Mathf.Sin(noiseValueB * 2.0f * Mathf.PI);
+            offsetPos.y = Mathf.Sin(noiseValueA * 2.0f * Mathf.PI) * Mathf.Sin(noiseValueB * 2.0f * Mathf.PI);
+            offsetPos.z = Mathf.Cos(noiseValueB * 2.0f * Mathf.PI);
 
-            Vector3 currentPos = new Vector3(curSegmentScreenPos.x + offsetPos.x, worm.position.y, curSegmentScreenPos.y + offsetPos.y);
+            Vector3 currentPos = curSegmentScreenPos + offsetPos;
 
             GameObject wormSegment = Instantiate(worm.gameObject, currentPos, worm.rotation);
             wormSegments.Add(wormSegment);
