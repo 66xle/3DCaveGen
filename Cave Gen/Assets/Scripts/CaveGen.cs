e3837d91b1ca243d4127ab911a49fc4f444d8cc0
Samuel <robbo_rules1@live.com.au>
2021-04-29 11:57:54 +1000

    Created Cave Block

--- Cave Gen/Assets/Scripts/CaveGen.cs
+++ Cave Gen/Assets/Scripts/CaveGen.cs
@@ -8,13 +8,39 @@ using LibNoise.Unity.Operator;
 
 public class CaveGen : MonoBehaviour
 {
-    public int mapSizeX;
-    public int mapSizeY;
-    public int mapSizeZ;
+    public GameObject block;
 
-    public float x;
-    public float y;
-    public float z;
+    public int mapSizeX = 10;
+    public int mapSizeY = 10;
+    public int mapSizeZ = 10;
 
-    
+    public float blockSize = 1.0f;
+
+    void Start()
+    {
+        Vector3 spawnBlock = new Vector3(-(mapSizeX / 2), mapSizeZ / 2, mapSizeY / 2);
+        for (int y = 0; y < mapSizeX; y++)
+        {
+            for (int z = 0; z < mapSizeX; z++)
+            {
+                for (int x = 0; x < mapSizeX; x++)
+                {
+                    GameObject wall = Instantiate(block, spawnBlock, block.transform.rotation);
+                    wall.transform.localScale = new Vector3(blockSize, blockSize, blockSize);
+                    wall.transform.SetParent(transform);
+
+                    spawnBlock.x += blockSize;
+                }
+                spawnBlock.x -= blockSize * mapSizeX;
+                spawnBlock.z -= blockSize;
+            }
+            spawnBlock.z += blockSize * mapSizeZ;
+            spawnBlock.y -= blockSize;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
 }
