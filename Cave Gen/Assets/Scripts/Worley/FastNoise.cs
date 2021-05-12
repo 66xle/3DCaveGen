31ce749051af7c39b9155c75c875d859cb7f5844
Samuel <robbo_rules1@live.com.au>
2021-05-12 17:08:54 +1000

    save scripts lel

---
+++ Cave Gen/Assets/Scripts/Worley/FastNoise.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FastNoise
+{
+    private int seed;
+    private float frequency = 0.01f;
+
+    private int octaves = 3;
+    private float lacunarity = 2.0f;
+    private float gain = 0.5f;
+
+    private float fractalBounding;
+
+    public FastNoise(int s)
+    {
+        seed = s;
+        CalculateFractalBounding();
+    }
+
+    public void SetFrequency(float f)
+    {
+        frequency = f;
+    }
+
+    private void CalculateFractalBounding()
+    {
+        float amp = gain;
+        float ampFractal = 1;
+        for (int i = 1; i < octaves; i++)
+        {
+            ampFractal += amp;
+            amp *= gain;
+        }
+
+        fractalBounding = 1 / ampFractal;
+    }
+
+    public float GetNoise(float x, float y)
+    {
+        x *= frequency;
+        y *= frequency;
+
+        // NoiseType: Perlin
+        int x0 = FastFloor(x);
+        int y0 = FastFloor(y);
+        int x1 = x0 + 1;
+        int y1 = y0 + 1;
+
+        // Interp: Quintic
+        float xs, ys;
+        xs = InterpQuinticFunc(x - x0);
+        ys = InterpQuinticFunc(y - y0);
+
+        float xd0 = x - x0;
+        float yd0 = y - y0;
+        float xd1 = xd0 - 1;
+        float yd1 = yd0 - 1;
+
+        float xf0 = Lerp(GradCoord2D(seed, x0, y0, xd0, yd0), GradCoord2D(seed, x1, y0, xd1, yd0), xs);
+        float xf1 = Lerp(GradCoord2D(seed, x0, y1, xd0, yd1), GradCoord2D(seed, x1, y1, xd1, yd1), xs);
+
+        return Lerp(xf0, xf1, ys);
+    }
+
+    public int FastFloor(float f)
+    {
+        return (f >= 0 ? (int)f : (int)f - 1);
+    }
+
+    float InterpQuinticFunc(float t)
+    {
+        return t * t * t * (t * (t * 6 - 15) + 10);
+    }
+
+    float Lerp(float a, float b, float t)
+    {
+        return a + t * (b - a);
+    }
+
+    // Hashing
+    public const int X_PRIME = 1619;
+    public const int Y_PRIME = 31337;
+    public const int Z_PRIME = 6971;
+    public const int W_PRIME = 1013;
+
+    float GradCoord2D(int seed, int x, int y, float xd, float yd)
+    {
+        int hash = seed;
+        hash ^= X_PRIME * x;
+        hash ^= Y_PRIME * y;
+
+        hash = hash * hash * hash * 60493;
+        hash = (hash >> 13) ^ hash;
+
+        Vector2 g = GRAD_2D[hash & 7];
+
+        return xd * g.x + yd * g.y;
+    }
+
+    private Vector2[] GRAD_2D = { new Vector2(-1, -1), new Vector2(1, -1), new Vector2(-1, 1), new Vector2(1, 1), new Vector2(0, -1), new Vector2(-1, 0), new Vector2(0, 1), new Vector2(1, 0), };
+}
---
+++ Cave Gen/Assets/Scripts/Worley/WorleyCave.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorleyCave : MonoBehaviour
+{
+    const int HAS_CAVES_FLAG = 129;
+
+    public int seed = 0;
+    //public float frequency = 0.05f;
+
+    public int minCaveHeight = 0;
+    public int maxCaveHeight = 100;
+
+    public float noiseCutoff;
+    public float warpAmplifier = 1.0f;
+    public float yCompression;
+    public float xzCompression;
+
+
+    private WorleyUtil util;
+    private FastNoise noisePerlin;
+
+
+    void Setup()
+    {
+        util = new WorleyUtil(seed);
+        util.SetFrequency(0.016f);
+
+        noisePerlin = new FastNoise(seed);
+        noisePerlin.SetFrequency(0.05f);
+
+        CarveWorleyCaves(0, 0);
+    }
+
+    void CarveWorleyCaves(int chunkX, int chunkZ)
+    {
+        int chunkMaxHeight = maxCaveHeight;
+        float[,,] samples = SampleNoise(chunkX, chunkZ, chunkMaxHeight + 1);
+        float oneQuarter = 0.25f;
+        float oneHalf = 0.5f;
+        Vector3 localPos;
+        Vector3 realPos;
+
+
+        // each chunk divided into 4 subchunks along X axis
+        for (int x = 0; x < 4; x++)
+        {
+            // each chunk divided into 4 subchunks along Z axis
+            for (int z = 0; z < 4; z++)
+            {
+                int depth = 0;
+
+                //don't bother checking all the other logic if there's nothing to dig in this column
+                if (samples[x, HAS_CAVES_FLAG, z] == 0 && samples[x + 1, HAS_CAVES_FLAG, z] == 0 && samples[x, HAS_CAVES_FLAG, z + 1] == 0 && samples[x + 1, HAS_CAVES_FLAG, z + 1] == 0)
+                    continue;
+
+                // each chunk divided into 128 subchunks along Y axis. Need lots of y sample points to not break things
+                for (int y = (maxCaveHeight / 2) - 1; y >= 0; y--)
+                {
+                    // grab the 8 sample points needed from the noise values
+                    float x0y0z0 = samples[x,y,z];
+                    float x0y0z1 = samples[x,y,z + 1];
+                    float x1y0z0 = samples[x + 1,y,z];
+                    float x1y0z1 = samples[x + 1,y,z + 1];
+                    float x0y1z0 = samples[x,y + 1,z];
+                    float x0y1z1 = samples[x,y + 1,z + 1];
+                    float x1y1z0 = samples[x + 1,y + 1,z];
+                    float x1y1z1 = samples[x + 1,y + 1,z + 1];
+
+                    // how much to increment noise along y value linear interpolation from start y and end y
+                    float noiseStepY00 = (x0y1z0 - x0y0z0) * -oneHalf;
+                    float noiseStepY01 = (x0y1z1 - x0y0z1) * -oneHalf;
+                    float noiseStepY10 = (x1y1z0 - x1y0z0) * -oneHalf;
+                    float noiseStepY11 = (x1y1z1 - x1y0z1) * -oneHalf;
+
+                    // noise values of 4 corners at y=0
+                    float noiseStartX0 = x0y0z0;
+                    float noiseStartX1 = x0y0z1;
+                    float noiseEndX0 = x1y0z0;
+                    float noiseEndX1 = x1y0z1;
+
+                    // loop through 2 blocks of the Y subchunk
+                    for (int suby = 1; suby >= 0; suby--)
+                    {
+                        int localY = suby + y * 2;
+                        float noiseStartZ = noiseStartX0;
+                        float noiseEndZ = noiseStartX1;
+
+                        // how much to increment X values, linear interpolation
+                        float noiseStepX0 = (noiseEndX0 - noiseStartX0) * oneQuarter;
+                        float noiseStepX1 = (noiseEndX1 - noiseStartX1) * oneQuarter;
+
+                        for (int subx = 0; subx < 4; subx++)
+                        {
+                            int localX = subx + x * 4;
+
+                            // how much to increment Z values, linear interpolation
+                            float noiseStepZ = (noiseEndZ - noiseStartZ) * oneQuarter;
+
+                            // Y and X already interpolated, just need to interpolate final 4 Z block to get final noise value
+                            float noiseVal = noiseStartZ;
+
+                            for (int subz = 0; subz < 4; subz++)
+                            {
+                                int localZ = subz + z + 4;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    float[,,] SampleNoise(int chunkX, int chunkZ, int maxSurfaceHeight)
+    {
+        int originalMaxHeight = 128;
+        float[,,] noiseSamples = new float[5, 130, 5];
+        float noise;
+
+        for (int x = 0; x < 5; x++)
+        {
+            int realX = x * 4 + chunkX * 16;
+            for (int z = 0; z < 5; z++)
+            {
+                int realZ = z * 4 + chunkZ * 16;
+                bool columnHasCaveFlag = false;
+
+                // loop from top down for y values so we can adjust noise above current y later on
+                for (int y = 128; y >= 0; y--)
+                {
+                    float realY = y * 2;
+                    if (realY > maxSurfaceHeight || realY > maxCaveHeight || realY < minCaveHeight)
+                    {
+                        // if outside of valid cave range set noise value below normal minimum of -1.0
+                        noiseSamples[x, y, z] = -1.1f;
+                    }
+                    else
+                    {
+                        // Experiment making the cave system more chaotic the more you descend
+                        float dispAmp = warpAmplifier * ((originalMaxHeight - y) / (originalMaxHeight * 0.85f));
+
+                        float xDisp = noisePerlin.GetNoise(realX, realZ) * dispAmp;
+                        float yDisp = noisePerlin.GetNoise(realX, realZ + 67.0f) * dispAmp;
+                        float zDisp = noisePerlin.GetNoise(realX, realZ + 149.0f) * dispAmp;
+
+                        // doubling the y frequency to get some more caves
+                        noise = util.SingleCellular3Edge(realX * xzCompression + xDisp, realY * yCompression + yDisp, realZ * xzCompression + zDisp);
+                        noiseSamples[x, y, z] = noise;
+
+                        if (noise > noiseCutoff)
+                        {
+                            columnHasCaveFlag = true;
+                            // if noise is below cutoff, adjust values of neighbors helps prevent caves fracturing during interpolation
+                            if (x > 0)
+                                noiseSamples[x - 1, y, z] = (noise * 0.2f) + (noiseSamples[x - 1, y, z] * 0.8f);
+                            if (z > 0)
+                                noiseSamples[x, y, z - 1] = (noise * 0.2f) + (noiseSamples[x, y, z - 1] * 0.8f);
+
+                            // more heavily adjust y above 'air block' noise values to give players more head room
+                            if (y < 128)
+                            {
+                                float noiseAbove = noiseSamples[x, y + 1, z];
+                                if (noise > noiseAbove)
+                                    noiseSamples[x, y + 1, z] = (noise * 0.8F) + (noiseAbove * 0.2F);
+
+                                if (y < 127)
+                                {
+                                    float noiseTwoAbove = noiseSamples[x, y + 2, z];
+                                    if (noise > noiseTwoAbove)
+                                        noiseSamples[x, y + 2, z] = (noise * 0.35F) + (noiseTwoAbove * 0.65F);
+                                }
+                            }
+                        }
+                    }
+                }
+                noiseSamples[x, HAS_CAVES_FLAG, z] = columnHasCaveFlag ? 1 : 0;
+            }
+            
+        }
+        return noiseSamples;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
