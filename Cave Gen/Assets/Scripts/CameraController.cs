e7421cb5708d445b1d460f3e2719464bede6327c
Samuel <robbo_rules1@live.com.au>
2021-05-14 09:10:04 +1000

    Added player

---
+++ Cave Gen/Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float minX = -60f;
+    public float maxX = 60;
+    public float minY = -360;
+    public float maxY = 360;
+
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+
+    public Camera cam;
+
+    float rotX = 0f;
+    float rotY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        rotX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+        rotX = Mathf.Clamp(rotX, minX, maxX);
+
+        transform.localEulerAngles = new Vector3(0, rotY, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotX, rotY, 0);
+
+    }
+}
---
+++ Cave Gen/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed;
+
+    Rigidbody rb;
+    Vector3 moveDir;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        moveDir = (horizontal * transform.right + vertical * transform.forward).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 yVelFix = new Vector3(0, rb.velocity.y, 0);
+        rb.velocity = moveDir * walkSpeed * Time.deltaTime;
+        rb.velocity += yVelFix;
+    }
+}
